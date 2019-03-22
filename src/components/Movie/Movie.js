a7254400148a7c36d2d78de5329263b213de4157
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-03-22 16:02:29 +0100

    Changing class components to functions

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import styles from './Movie.style'
 
-export default class Movie extends Component {
-  render() {
-    const { movie } = this.props
-
-    return (
-      <li className={styles.container}>
-        <h2 className={styles.title}>{movie.title}</h2>
-        <span className={styles.year}>Year: {movie.year}</span>
-        <img
-          className={styles.image}
-          src={movie.poster}
-          alt={`Poster for ${movie.title}`}
-        />
-        <p className={styles.synopsis}>{movie.synopsis}</p>
-      </li>
-    )
-  }
+const Movie = ({ movie }) => {
+  return (
+    <li className={styles.container}>
+      <h2 className={styles.title}>{movie.title}</h2>
+      <span className={styles.year}>Year: {movie.year}</span>
+      <img
+        className={styles.image}
+        src={movie.poster}
+        alt={`Poster for ${movie.title}`}
+      />
+      <p className={styles.synopsis}>{movie.synopsis}</p>
+    </li>
+  )
 }
+
+export default Movie
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Movie from '../Movie'
 
 import styles from './MovieList.style'
 
-export default class MovieList extends Component {
-  render() {
-    const { movies } = this.props
-    return (
-      <ul className={styles.list}>
-        {movies.map(movie => (
-          <Movie key={movie.title} movie={movie} />
-        ))}
-      </ul>
-    )
-  }
+const MovieList = ({ movies }) => {
+  return (
+    <ul className={styles.list}>
+      {movies.map(movie => (
+        <Movie key={movie.title} movie={movie} />
+      ))}
+    </ul>
+  )
 }
+
+export default MovieList
